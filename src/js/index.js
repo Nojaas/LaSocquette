da407effb765ca7f8a79050d8ea97777fb1bc5b7
nbinet <nbinet@DESKTOP-E09GB24.localdomain>
2020-02-06 11:00:25 +0100

    waypoint

---
+++ src/js/index.js
@@ -0,0 +1,14 @@
+var ids = ['article2', 'article3', 'article4', 'article5', 'article6'];
+
+ids.forEach(function(id) {
+  var element = document.getElementById(id);
+  console.log(id);
+
+  var waypoint = new Waypoint({
+    element: element,
+    handler: function(direction) {
+      element.classList.add('is-reached');
+    },
+    offset: '75%'
+  });
+});
